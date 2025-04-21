206d51aab24b41d6abcf523595b8832edfe01f7e
XiaoYu <weitsungyu@gmail.com>
2025-04-21 09:32:35 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button>
    
    with the text content:
    
        Register
    
    Relevant files:
    
        - https://6000-idx-studio-1745222402173.cluster-zumahodzirciuujpqvsniawo3o.cloudworkstations.dev/_next/static/chunks/_dc51ab85._.js
        - https://6000-idx-studio-1745222402173.cluster-zumahodzirciuujpqvsniawo3o.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-idx-studio-1745222402173.cluster-zumahodzirciuujpqvsniawo3o.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    這個按鈕刪掉

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import LoginForm from "@/components/login-form";
-import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
@@ -11,10 +10,6 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-3xl font-bold mb-6">Welcome to LoginSnake</h1>
       <LoginForm />
-      <Button onClick={() => router.push("/register")}>
-        Register
-      </Button>
     </div>
   );
 }
-
